953be463551605bd740a0920513a86485d480f9d
Matthew-Stormblessed <matthewisaacjohnson@gmail.com>
2026-07-27 16:38:23 -0600

    Update file retrieval

--- app/api/getProjectData/route.ts
+++ app/api/getProjectData/route.ts
@@ -1,12 +1,30 @@
 import { NextResponse } from "next/server";
 import fs from "fs";
+import portfolioAssistant from "@/app/data/PortfolioAssistant.json";
+import popChoice from "@/app/data/PopChoice.json";
+import travelAgent from "@/app/data/TravelAgent.json";
+import pollyglot from "@/app/data/Pollyglot.json";
 
 
 export async function POST(request: Request) {
     try {
         const requestData = await request.json();
-        const data = fs.readFileSync(requestData, "utf8");
-        const parsedData = JSON.parse(data);
+        let parsedData;
+
+        switch (requestData) {
+            case "PortfolioAssistant.json":
+                parsedData = portfolioAssistant;
+                break;
+            case "PopChoice.json":
+                parsedData = popChoice;
+                break;
+            case "TravelAgent.json":
+                parsedData = travelAgent;
+                break;
+            default:
+                return NextResponse.json({ error: "Invalid project data request" }, { status: 400 });
+        }
+
         return NextResponse.json(parsedData);
     } catch (error) {
         console.error("Error processing request:", error);
